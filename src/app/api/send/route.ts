a2cce4de2e92863c49f0fc682872d758d9ea624e
Vaibhav Yadav <vaibhav-buildz@users.noreply.github.com>
2026-03-27 14:53:40 +0530

    fix(api): instantiate Resend client inside handler

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,14 +3,13 @@ import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const Email = z.object({
   fullName: z.string().min(2, "Full name is invalid!"),
   email: z.string().email({ message: "Email is invalid!" }),
   message: z.string().min(10, "Message is too short!"),
 });
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body = await req.json();
     console.log(body);
